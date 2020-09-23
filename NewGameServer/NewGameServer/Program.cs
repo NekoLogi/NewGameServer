00bb10ba4a47e4f3e03a66abf69f3b1e19b0679b
NekoLogi <NekoLogi@github.com>
2020-09-23 16:26:59 +0200

    Created

---
+++ NewGameServer/NewGameServer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NewGameServer
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
